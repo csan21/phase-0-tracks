c65dbfd3457c1a6116c971abf4a50c23c4b8cb80
csan21 <chrisswho@gmail.com>
2016-10-27 18:20:05 -0500

    Start on release two conditions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,27 @@
 puts "What is your name?"
+	name = gets.chomp
 
 puts "How old are you? What year were you born?"
+	age = gets.chomp
+	birth_year = gets.chomp
+	current_year = 2016
+
+	if #{current_year} - #{birth_year} == #{age}
+		age_correct = true
+	end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic_bread = gets.chomp
+
+	if garlic_bread == "yes"
+	end
 
 puts "Would you like to enroll in the company's health insurance?"
+	health_ins = gets.chomp
+
+	if health_ins == "yes"
+	end
+
+if age_correct && garlic_bread && health_ins
+	puts "Probably not a vampire"
+end
