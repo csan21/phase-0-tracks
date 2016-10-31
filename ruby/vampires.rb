5baf2d6bed0cd0e0d15979e165a6fdf3e1a43bcd
csan21 <chrisswho@gmail.com>
2016-10-31 06:53:37 -0500

    Add corrected loops and allergy test

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,9 +1,17 @@
 puts "How many employees will be processed?"
 	employee_count = gets.chomp.to_i
-	while counter < employee_count
+	counter = 0
+	until counter == employee_count
 
 		puts "What is your name?"
 			name = gets.chomp
+			vamp_name = false
+
+			if name == "Drake Cula"
+				vamp_name = true
+			elsif name == "Tu Fang"
+				vamp_name = true
+			end
 
 		puts "How old are you? What year were you born?"
 			age = gets.chomp.to_i
@@ -34,15 +42,37 @@ puts "How many employees will be processed?"
 				health_ins = false
 			end
 
-	counter += 1
-	end
+		puts "Please list any allergies you have, one at a time. Type done when finished"
+			allergies = ""
+			allergies_list = 0
 
-if correct_age && (likes_garlic || health_ins) == true
-	puts "Probably not a vampire"
-elsif correct_age || likes_garlic || health_ins == true
-	puts "Probably a vampire"
-elsif correct_age || likes_garlic || health_ins == false
-	puts "Almost certainly a vampire."
-else 
-	puts "Results are inconclusive"
+			while allergies_list < 1
+				allergies = gets.chomp
+				if allergies == "done"
+					allergies_list += 1
+				elsif allergies_list == "sunshine"
+					allergies_list += 1
+					allergy_vamp = true
+			end
+
+
+		if vamp_name == true
+			puts "Definitely a vampire"
+		elsif allergy_vamp == true
+			puts "Probably a vampire"
+		elsif correct_age && (likes_garlic || health_ins) == true
+			puts "Probably not a vampire"
+		elsif correct_age || likes_garlic || health_ins == true
+			puts "Probably a vampire"
+		elsif correct_age || likes_garlic || health_ins == false
+			puts "Almost certainly a vampire."
+		else 
+			puts "Results are inconclusive"
+		end
+
+	counter += 1
+	
+	end
 end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends!"
