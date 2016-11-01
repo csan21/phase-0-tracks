d31c474829ec9912ed4f0779dae27a9295b4f5b2
csan21 <chrisswho@gmail.com>
2016-11-01 15:04:44 -0500

    Initial commit with color array and capital hash

---
+++ ruby/iteration.rb
@@ -0,0 +1,17 @@
+# def greeting
+# 	name1 = "TJ"
+# 	name2 = "Rj"
+# 	name3 = "Bj"
+# 	puts "Hello there."
+# 	yield (name1, name2, name3)
+# end
+
+# greeting { |name1, name2, name3| puts "thank the sun!" }
+
+colors = {"red", "green", "blue"}
+
+state_capitals = {
+	illinois: "Springfield",
+	wisconsin: "Madison",
+	arizona: "Phoenix"
+}
