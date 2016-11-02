e981e882f7e3a2d40f047a03d6afaf6fff3c135c
csan21 <chrisswho@gmail.com>
2016-11-02 16:53:20 -0500

    Fix readability and direction errors

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -10,35 +10,34 @@ puts "Enter client's name:"
 	name = gets.chomp
 
 puts "Enter client's age:"
-	age = gets.chomp
+	age = gets.chomp.to_i
 
 puts "Enter number of children in household:"
-	children = gets.chomp.downcase
+	children = gets.chomp.to_i
 
-		if children == "none"
-			children = ""
-		end
+# answer = false
 
-answer = false
-
-until answer == true
+# until answer == true
 
 puts "Enter decor theme:"
 	decor = gets.chomp
 
-		if decor == "decor_theme"
-			answer = false
-			puts "Enter a new value"
-		else 
-			answer = true
-		end
-end
+# 		if decor == "decor_theme"
+# 			answer = false
+# 			puts "Enter a new value"
+# 		else 
+# 			answer = true
+# 		end
+# end
 
 information = {
-	name: "#{name}",
-	age: "#{age}",
-	children: "#{children}",
-	decor: "#{decor}",
+	name: name,
+	age: age,
+	children: children,
+	decor: decor,
 }
 
 p information
+
+puts "If you would like to update a section type name/age/children/or decor."
+puts "If the information is correct, please type 'none' to exit"
