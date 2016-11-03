9f96d5d15923aaf2d35555e2d30da4558bcb4402
csan21 <chrisswho@gmail.com>
2016-11-03 12:58:36 -0500

    Add release 1

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -12,3 +12,28 @@ def searchArray(arr, num)
  
 arr = [2, 4, 7, 13, 76, 5, 1, 3]
 searchArray(arr, 13)
+
+# Release 1
+
+def add(num1, num2)
+        final = num1 + num2
+        return final
+end
+
+
+def fib(num)
+    arr = [0, 1]
+    
+    index = 0
+    while index < num - 2
+        arr << add(arr[index], arr[index + 1])
+        index += 1
+    end
+    
+    return arr    
+end
+
+newarr = fib(6)
+p newarr
+
+
