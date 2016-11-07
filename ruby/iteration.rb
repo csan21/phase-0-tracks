5eea50d986c86904e6b98d5216b662835975898c
csan21 <chrisswho@gmail.com>
2016-11-06 22:45:18 -0600

    Update release 0 and 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,28 +1,35 @@
-# def greeting
-# 	name1 = "TJ"
-# 	name2 = "Rj"
-# 	name3 = "Bj"
-# 	puts "Hello there."
-# 	yield (name1, name2, name3)
-# end
+#///// Release 0 + 1
 
-# greeting { |name1, name2, name3| puts "thank the sun!" }
+def greeting
+	name1 = "TJ"
+	name2 = "Rj"
+	name3 = "Bj"
+	yield(name1, name2, name3)
+end
 
-# colors = ["red", "green", "blue"]
+greeting { |name1, name2, name3| puts "#{name1}, #{name2}, #{name3} thank the sun!" }
 
-# state_capitals = {
-# 	illinois: "Springfield",
-# 	wisconsin: "Madison",
-# 	arizona: "Phoenix"
+colors = ["red", "green", "blue"]
+
+state_capitals = {
+	illinois: "Springfield",
+	wisconsin: "Madison",
+	arizona: "Phoenix"
 }
 
-# color.each { |color| puts "Likes the color #{color}" }
+colors.each { |color| puts "Likes the color #{color}" }
 
-# colors.map! do |color|
-# 	puts color
-# 	color.reverse
-# end
+colors.map! do |color|
+	puts color
+	color.reverse
+end
 
-# p color
+p colors
 
-# state_capitals.each { |state_capitals| puts "Lets visit #{state_capitals"!}
+p state_capitals.each { |state_capitals| puts "Lets visit #{state_capitals}!" }
+
+#///// Release 2
+
+
+
+
