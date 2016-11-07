ea35dbbf609d6b64b571f95cc42db0dbb7098221
csan21 <chrisswho@gmail.com>
2016-11-06 22:27:23 -0600

    Complete releases

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,12 +1,27 @@
-favorite_artists {
-	'hip-hop' => [
-		'J.Cole',
+favorite_artists = {
+	hip_hop: [
+		"J.Cole",
 		'Nas'
-	]
-	'dance' => [
-		'Armin Van Buuren'
-	]
-	'rock' => [
-		'Metallica'
-	]
+	],
+	dance: [
+		"Armin Van Buuren",
+		"Andrew Rayel"
+	],
+	rock: [
+		"Metallica",
+		"Sevendust"
+	],
+	festivals_attended: {
+		next_year: 5,
+		this_year: 4,
+		last_year: 3
+	}
 }
+
+p favorite_artists[:rock][1]
+
+p favorite_artists[:festivals_attended][:this_year]
+
+favorite_artists[:dance].push("Above and Beyond")
+
+p favorite_artists[:dance][2]
