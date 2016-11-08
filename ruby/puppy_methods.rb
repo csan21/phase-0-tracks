29a8a132edb8a06ceeca65c2ed408b00eb705adf
csan21 <chrisswho@gmail.com>
2016-11-08 13:54:18 -0600

    Initial commit - Release 1 to 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,58 @@
 class Puppy
 
+  def initialize()
+  	puts "New dog"
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(int)
+  	int.times { puts "woof"}
+  end
+
+  def rollover
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_age)
+  	dog_year = 7 * human_age
+  	p dog_year
+  end
+
 end
 
+class Person
+
+	def initialize(age, gender, name)
+		@name = name
+		@age = age
+		@gender = gender
+		puts "New person!"
+	end
+
+	def birthday
+		@age += 1
+	end
+
+
+
+end
+
+shibainu = Puppy.new
+
+shibainu.fetch("ball")
+shibainu.speak(5)
+shibainu.rollover
+shibainu.dog_years(28)
+
+names = ['Mhar', 'Chris', 'Danny', 'Ashley', 'Rick', 'Bob']
+people = []
+gender = ['Male', 'Female']
+age = [0..30].to_a
+
+50.times { people << Person.new(names.sample, age.sample, gender.sample) }
+
+p people
