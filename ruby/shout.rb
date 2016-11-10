ad358d41304ae405bc63d955ac8449f08c684657
csan21 <chrisswho@gmail.com>
2016-11-10 14:50:08 -0600

    Complete Release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,35 @@
 module Shout
-	def self.yell_angrily(words)
-    	words + "!!!" + " :("
+
+	def scream(words)
+		puts words + "!!!!!"
 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
-	end
 end
 
-Shout.yell_angrily("GO AWAY")
-Shout.yelling_happily("COME BACK")
+class Person
+	include Shout
+end
+
+class Dog
+	include Shout
+end
+
+person = Person.new
+person.scream('FETCH IT')
+
+dog = Dog.new
+dog.scream('NO THANKS')
+
+# module Shout
+
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
+
+# Shout.yell_angrily("GO AWAY")
+# Shout.yelling_happily("COME BACK")
