d8fe9e20ac0f233d53c200980cbbf00c2f02f3cd
csan21 <chrisswho@gmail.com>
2016-11-14 04:33:08 -0600

    Add notes above pseudocode

--- ruby/week6game/game.rb
+++ ruby/week6game/game.rb
@@ -1,3 +1,11 @@
+# //// 6.6 note ////
+# I got stuck and couldnt return the letters from the hidden "_"
+# I tried tons of variations with arrays and without
+# To no avail.
+# Since I couldn't have the letters replace the "_" , i couldn't set a win condition
+
+
+
 ## A Guessing Game ##
 
 # Pseudo #
@@ -12,37 +20,43 @@
 	# If Player 2 loses, taunting message
 
 class GuessGame
-	attr_reader :word, :hidden, :game_over
+	attr_reader :word, :game_over
 
 # Initializes values and hides the word
 	def initialize(word)
+		puts "New game starting now"
 		@word = word
-		@hidden = word.split('')
-		@hidden.map! {|w| w = "_"}
 		@guess_count = 0
 		@game_over = false
 	end
 
-# Maps out the letters hidden by "_"
-	def test_area
-		@hidden.each{|w| print " #{w} "}
+# Takes the Player 1 word and hides it
+	def hidden_word(word, char)
+		@hide = ''
+		word.chars { |letter|
+			@hide += (char.include? letter)? letter : "_" }
+		@hide
 	end
 
 # Checks player 2's input aka letter guesses
-	def user_guess(letter)
-		if @word.include? (letter)
+	def user_guess(guesser)
+		if @word.include? (guesser)
 			@guess_count += 1
-			puts "#{letter} is correct"
+			puts "#{guesser} is correct"
+			@hide = hidden_word(word, guesser)
+			p @hide
 		else
-			puts "#{letter} is incorrect"
-			@guess_count +=1
+			puts "#{guesser} is incorrect"
+			@guess_count += 1
 		end
 	end	
 
+# Set conditions for the game to end
 	def game_condition
 		if @guess_count == @word.length
 			@game_over = true
 			puts "YOU LOSE"
+		end		
 	end
 
 end
@@ -53,21 +67,16 @@ puts "Player 1 - ENTER A WORD:"
 input = gets.chomp.downcase
 
 game = GuessGame.new(input)
-
 puts `clear`
-
-# # PERSONAL TESTING AREA
-# # game.test_area
-
+print game.hidden_word(input, "_")
+puts
 puts "Player 2 - You have #{input.length} attempts so be careful"
 
 while !game.game_over
-	game.test_area
 	puts
 	puts "Player 2 - GUESS A LETTER:"
 	puts
-	letter = gets.chomp.downcase
-	game.user_guess(letter)
+	guesser = gets.chomp.downcase
+	game.user_guess(guesser)
 	game.game_condition
-end
 end
