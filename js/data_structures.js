59e1fbde3a6ea6dab66a1174a1a30391c8b9584b
csan21 <chrisswho@gmail.com>
2016-11-16 16:24:44 -0600

    Complete release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,3 +1,5 @@
+// Release 0
+
 var horseColors = ["blue", "red", "yellow", "green"]
 
 var horseNames = ["Edd", "Bob", "Bill", "Joe"]
@@ -7,3 +9,14 @@ horseNames.push("Jim")
 
 // console.log(horseColors)
 // console.log(horseNames)
+
+// Release 1
+
+var horse = {horseNames: horseColors};
+
+for (var i = 0; i < horseNames.length; i++) {
+	console.log(horseNames[i] + " is " + horse.horseNames[i]);
+}
+
+// console.log(horse.horseNames)
+// console.log(horse.horseColors)
