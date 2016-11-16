51492dcfe176b07e8db3963492af8a668fef75a5
csan21 <chrisswho@gmail.com>
2016-11-16 17:07:11 -0600

    Complete release 2 and module

--- js/data_structures.js
+++ js/data_structures.js
@@ -25,14 +25,34 @@ for (var i = 0; i < horseNames.length; i++) {
 
 function Car(make, model, year, isGoodCar) {
 
-	console.log("Our new car:", this);
 	this.make = make;
 	this.model = model;
 	this.year = year;
 	this.isGoodCar = isGoodCar;
+	console.log("Our new car:", this.make, this.model, this.year);
 
-	this.vroom = function() { console.log("vroom!"); };
+	this.vroom = function() { console.log("vrooOoom!"); };
 
 	console.log("CAR INITIALIZATION COMPLETE");
 }
+
+console.log("Let's build a car ...");
+
+var firstCar = new Car("Acura", "RSX", 2001, true)
+console.log(firstCar)
+console.log("This car is fast:")
+firstCar.vroom();
+console.log("-------")
+
+var secondCar = new Car("Subaru", "WRX", 2002, true)
+console.log(secondCar)
+console.log("This car is faster:")
+secondCar.vroom();
+console.log("-------")
+
+var thirdCar = new Car("Mitsubishi", "Evolution", 2007, true)
+console.log(thirdCar)
+console.log("This car is fastest:")
+thirdCar.vroom();
+console.log("-------")
 
