6abbc19c2c11bf32aee37a808352ad14fc50ac53
csan21 <chrisswho@gmail.com>
2016-11-20 21:36:21 -0600

    Add release 1 pseudo and code complete

--- js/algos.js
+++ js/algos.js
@@ -16,8 +16,33 @@ function longestInput(array) {
 	}
 }
 
+// Release 1
+
+// Function takes two objects
+// If objects share a key value pair, returns true
+// Else, returns false
+
+function checkObjects(object1, object2) {
+	if (object1.age == object2.age) {
+		return true
+	}
+	else {
+		return false
+	}
+}
+
 // Driver Release 0
 array = ["long phrase","longest phrase","longer phrase"]
 console.log(longestInput(array))
+
 array = ["Hello there people", "what are you doing", "what am i doing"]
 console.log(longestInput(array))
+
+// Driver Release 1
+var person1 = {name: "Steven", age: 54}
+var person2 = {name: "Tamir", age: 54}
+console.log(checkObjects(person1, person2))
+
+var person1 = {name: "Nevets", age: 60}
+var person2 = {name: "Rimat", age: 54}
+console.log(checkObjects(person1, person2))
