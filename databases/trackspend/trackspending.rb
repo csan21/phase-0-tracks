2509f2c3ead44a6f38bea9a8e8b2812d54906dbf
csan21 <chrisswho@gmail.com>
2016-11-28 01:26:57 -0600

    Add pseudocode

--- databases/trackspend/trackspending.rb
+++ databases/trackspend/trackspending.rb
@@ -1,3 +1,11 @@
 # PSEUDOCODE
 
+# ABOUT # A simple money spending tracker for me (or other dbc students) 
+# to use to monitor my spending habits while during the on site
+# portion. 
+
+# INPUT - a decimal representing currency in usd
+	# the input is added into a table.
+# OUTPUT - the total spent so far is updated for every new input.
+
 require 'sqlite3'
