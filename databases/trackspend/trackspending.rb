89960f5ba4b7e067c2361a5a30ff8da5b4d76a8f
csan21 <chrisswho@gmail.com>
2016-11-28 02:37:09 -0600

    Add two sql tests

--- databases/trackspend/trackspending.rb
+++ databases/trackspend/trackspending.rb
@@ -5,21 +5,37 @@
 
 # INPUT - a decimal representing currency in usd
 	# the first input creates a database for all inputs.
+	# inputs are added to database regardless of closing program
 # OUTPUT - the total spent so far is updated for every new input.
 
+# BUSINESS LOGIC
 require 'sqlite3'
 
 spent_db = SQLite3::Database.new("spent.db")
 
-create_table_cmd = <<-SQL
+create_table = <<-SQL
 	CREATE TABLE IF NOT EXISTS spent(
 		id INTEGER PRIMARY KEY,
 		cash DECIMAL
 	)
 SQL
 
-spent_db.execute(create_table_cmd)
+full_table = <<-SQL
+	SELECT * FROM spent;
+SQL
+
+total_spent = <<-SQL
+	SELECT ROUND(SUM(cash), 2) FROM spent;
+SQL
+
+spent_db.execute(create_table)
+full_list = spent_db.execute(full_table)
+total = spent_db.execute(total_spent)
 
 # TEST DB
-spent_db.execute("INSERT INTO spent (cash) VALUES (20.55)")
-spent_db.execute("INSERT INTO spent (cash) VALUES (8.25)")
+# spent_db.execute("INSERT INTO spent (cash) VALUES (20.55)")
+# spent_db.execute("INSERT INTO spent (cash) VALUES (8.25)")
+# puts "Total spent: #{total} USD"
+# p full_list
+
+# USER INTERFACE
