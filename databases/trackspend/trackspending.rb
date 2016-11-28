95d4cf8e2600588dc6f6bef85e4702a6d3ebea24
csan21 <chrisswho@gmail.com>
2016-11-28 03:11:07 -0600

    Add until loop for interface

--- databases/trackspend/trackspending.rb
+++ databases/trackspend/trackspending.rb
@@ -32,6 +32,10 @@ spent_db.execute(create_table)
 full_list = spent_db.execute(full_table)
 total = spent_db.execute(total_spent)
 
+# def input_check(input)
+# 	if input.include? decimal
+# end
+
 # TEST DB
 # spent_db.execute("INSERT INTO spent (cash) VALUES (20.55)")
 # spent_db.execute("INSERT INTO spent (cash) VALUES (8.25)")
@@ -39,3 +43,20 @@ total = spent_db.execute(total_spent)
 # p full_list
 
 # USER INTERFACE
+until valid
+
+	puts "Welcome to my DBC student money spending tracker! \n\n"
+	puts "- MENU -"
+	puts "Type 'total' to see your total spent"
+	puts "Type 'list' to see a list of your spendings in order"
+	puts "Type 'quit' to exit when you're finished adding \n\n"
+	puts "ENTER what you've spent today:"
+	input = gets.chomp.to_f
+
+end
+
+
+
+
+
+
