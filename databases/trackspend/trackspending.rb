8d66c11866716f0cc69ad710a6fdb6ddf2641937
csan21 <chrisswho@gmail.com>
2016-11-28 02:01:33 -0600

    Add database and test code

--- databases/trackspend/trackspending.rb
+++ databases/trackspend/trackspending.rb
@@ -8,3 +8,18 @@
 # OUTPUT - the total spent so far is updated for every new input.
 
 require 'sqlite3'
+
+spent_db = SQLite3::Database.new("spent.db")
+
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS spent(
+		id INTEGER PRIMARY KEY,
+		cash DECIMAL
+	)
+SQL
+
+spent_db.execute(create_table_cmd)
+
+# TEST DB
+spent_db.execute("INSERT INTO spent (cash) VALUES (20.55)")
+spent_db.execute("INSERT INTO spent (cash) VALUES (8.25)")
